18e6b35bcacf9e04600ccbd6863096a149c4b773
Tyler Gilman <tylersgilman@gmail.com>
2024-07-18 20:07:12 -0400

    styling

--- views/components/navigation_templ.go
+++ views/components/navigation_templ.go
@@ -171,7 +171,7 @@ func MainNavBar(page_mode string) templ.Component {
 		var templ_7745c5c3_Var7 string
 		templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(page_mode)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/navigation.templ`, Line: 120, Col: 79}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/navigation.templ`, Line: 121, Col: 79}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 		if templ_7745c5c3_Err != nil {
