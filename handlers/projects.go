cb0f39d2f4cdb184eb808b348a7b33006dcf322a
Tyler Gilman <tylersgilman@gmail.com>
2024-07-20 18:46:11 -0400

    tooltips and small home changes

--- handlers/projects.go
+++ handlers/projects.go
@@ -6,7 +6,7 @@ import (
 	"github.com/TylerGilman/nereus_main_site/views/projects"
 )
 
-func HandleFriends(w http.ResponseWriter, r *http.Request) error {
+func HandleProjects(w http.ResponseWriter, r *http.Request) error {
 	isHtmxRequest := r.Header.Get("HX-Request") == "true"
 	if isHtmxRequest {
 		return Render(w, r, projects.Partial())
