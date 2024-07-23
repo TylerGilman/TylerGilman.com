5f1602b5add98fe5b5bdf570480e25b934513260
Tyler Gilman <tylersgilman@gmail.com>
2024-07-23 15:03:25 -0400

    visuals

--- views/projects/projects_templ.go
+++ views/projects/projects_templ.go
@@ -185,7 +185,7 @@ func contributionDay(day models.ContributionDay) templ.Component {
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d contributions on %s", day.Count, day.Date))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/projects/projects.templ`, Line: 97, Col: 74}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/projects/projects.templ`, Line: 104, Col: 74}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
