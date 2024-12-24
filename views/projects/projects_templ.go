f6ca212025f1cb2b59e5818bdad8390df5709dc4
TylerGilman <tylersgilman@gmail.com>
2024-12-24 15:15:33 -0500

    resizing fishtank

--- views/projects/projects_templ.go
+++ views/projects/projects_templ.go
@@ -197,7 +197,7 @@ func contributionDay(day models.ContributionDay) templ.Component {
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d contributions on %s", day.Count, day.Date))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/projects/projects.templ`, Line: 161, Col: 74}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/projects/projects.templ`, Line: 173, Col: 74}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
